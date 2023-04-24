ee0f568f6327db79c63524473888c0ddfaafcfc9
Nathan Cheng <68567267+RoombaZoomba@users.noreply.github.com>
2023-04-23 21:13:29 -0500

    Update spinlock.c

--- kernel/spinlock.c
+++ kernel/spinlock.c
@@ -1,1 +1,24 @@
+#include <stdatomic.h>
+#include "spinlock.h"
+
+
+void spinlock_init(spinlock_t *lock) {
+    atomic_flag_clear(&lock->flag);
+}
+
+void spinlock_lock(spinlock_t *lock) {
+    while (atomic_flag_test_and_set(&lock->flag));
+}
+
+void spinlock_unlock(spinlock_t *lock) {
+    atomic_flag_clear(&lock->flag);
+}
+
+
+
+
+
+
+
+
 
